59f14b754e962ceee77ec15b5c3ff169d800bd85
md Ruhul <ruhulamin122017@gmail.com>
2023-01-15 19:51:36 +0600

    binding add

--- app/src/main/java/com/ruhul/quickpagingdemo/MainActivity.kt
+++ app/src/main/java/com/ruhul/quickpagingdemo/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.ruhul.quickpagingdemo.databinding.ActivityMainBinding
 import com.ruhul.quickpagingdemo.paging.QuoteLoadAdapter
 import com.ruhul.quickpagingdemo.paging.QuotePagingAdapter
 import com.ruhul.quickpagingdemo.viewmodels.QuoteViewModel
@@ -14,23 +15,23 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     lateinit var quoteViewModel: QuoteViewModel
-    lateinit var recyclerView: RecyclerView
     lateinit var adapter: QuotePagingAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        recyclerView = findViewById(R.id.quoteList)
-
-        quoteViewModel = ViewModelProvider(this).get(QuoteViewModel::class.java)
+        quoteViewModel = ViewModelProvider(this)[QuoteViewModel::class.java]
 
         adapter = QuotePagingAdapter()
 
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.setHasFixedSize(true)
-        recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
+        binding.RV.layoutManager = LinearLayoutManager(this)
+        binding.RV.setHasFixedSize(true)
+        binding.RV.adapter = adapter.withLoadStateHeaderAndFooter(
             QuoteLoadAdapter(),
             QuoteLoadAdapter()
         )
